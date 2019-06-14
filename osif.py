cd45151c0a9fdc65161d073004523aaf29a3ad9f
storiku <50206225+storiku@users.noreply.github.com>
2019-06-14 10:53:16 +0700

    Update osif.py

--- osif.py
+++ osif.py
@@ -73,14 +73,13 @@ def show_program():
                     %sINFORMATION%s
  ------------------------------------------------------
 
-    Author     Debby Anggraini 'CiKu370'
+    Author     Kunyim  'termux.id'
     Name       OSIF 'Open Source Information Facebook'
-    CodeName   D3b2y
+    CodeName   termux.id
     version    full version
     Date       16/05/2018 09:35:12
-    Team       Blackhole Security
-    Email      xnver404@gmail.com
-    Telegram   @CiKu370
+    Website    http://termux.id
+    
 
 * if you find any errors or problems , please contact
   author
